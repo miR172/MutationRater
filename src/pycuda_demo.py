deef6133b9688c08db245baf1dc27719f36da824
miR172 <flyhigh1990@gmail.com>
2014-11-24 14:03:39 -0500

    Usable demo for pycuda remember to use numpy.type

--- src/pycuda_demo.py
+++ src/pycuda_demo.py
@@ -6,25 +6,26 @@ import numpy
 
 add = SourceModule("""
 __global__ void add_kernel(int * a, int * b, int *c){
-  int i = threadIdx.y*threadIdx.y + threadIdx.x;
-  c[i] = i;
+  int i = threadIdx.x;
+  c[i] = a[i] + b[i];
 }
 """)
 msize = 32
 add_k = add.get_function("add_kernel")
-a = numpy.array(range(0, msize)).astype(int)
-b = numpy.array(range(msize, 0, -1)).astype(int)
-c = numpy.array([1]*msize).astype(int)
-# print "a.len:%d b.len:%d c.len:%d" %(a.size, b.size, c.size)
+a = numpy.array(range(0, msize)).astype(numpy.int32)
+b = numpy.array(range(msize, 0, -1)).astype(numpy.int32)
 
-c_zuo = cuda.mem_alloc(c.nbytes) # allocate, if not, use cuda.In and cuda.Out
+# output array allocate on device
+# the other way to allocate on device is to use cuda.In and cuda.Out
+c = numpy.array([0]*msize).astype(numpy.int32)
+c_zuo = cuda.mem_alloc(c.nbytes)
 cuda.memcpy_htod(c_zuo, c)
 
+# call the kernel
 add_k(cuda.In(a), cuda.In(b), c_zuo,
            block = (msize, 1, 1))
 
+# cpy back to host
 d = numpy.empty_like(c)
-print a.size, a
-print b.size, b
 cuda.memcpy_dtoh(d, c_zuo)
-print d.size, d
+print d.size, d # should be all 32
